a22c30a334b4cda8abefea3c1b052be62009d363
itmanharry <jack_chengqian@163.com>
2021-07-19 15:56:48 +0800

    add semantic ui

--- chat/views/auth.py
+++ chat/views/auth.py
@@ -1,6 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 #系统授权
 bp_auth = Blueprint('auth', __name__)
 @bp_auth.route('/')
 def index():
-    return '<h1>Auth View.</h1>'
+    return render_template('auth/login.html')
