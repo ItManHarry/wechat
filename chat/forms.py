cd4f2dd8601eb2d3af95b825e5b75901279a333e
itmanharry <jack_chengqian@163.com>
2021-08-14 16:53:47 +0800

    register finished

--- chat/forms.py
+++ chat/forms.py
@@ -1,7 +1,23 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms import ValidationError
+from chat.models import User
 class LoginForm(FlaskForm):
     code = StringField('账号',validators=[DataRequired('请输入用户名!'),Length(1, 20, '长度要介于(1-20)')])
     password = PasswordField('密码',validators=[DataRequired('请输入密码!'),Length(8, 128, '长度要介于(8-128)')])
     submit = SubmitField('登录')
+class RegisterForm(FlaskForm):
+    code = StringField('账号', validators=[DataRequired('请输入用户名!'), Length(1, 20, '长度要介于(1-20)')])
+    password = PasswordField('密码', validators=[DataRequired('请输入密码!'), Length(8, 128, '长度要介于(8-128)'), EqualTo('password_confirm', message='密码不一致')])
+    password_confirm = PasswordField('确认密码', validators=[DataRequired('请确认密码!')])
+    email = StringField('邮箱', validators=[DataRequired('请输入邮箱!'), Email('邮箱格式不正确')])
+    submit = SubmitField('注册')
+
+    def validate_code(self, field):
+        if User.query.filter_by(code=field.data.lower()).first():
+            raise ValidationError('账号已注册!')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data.lower()).first():
+            raise ValidationError('邮箱已注册!')
--- chat/views/auth.py
+++ chat/views/auth.py
@@ -1,12 +1,30 @@
 from flask import Blueprint, render_template, redirect, url_for, flash
 from flask_login import login_user, current_user, logout_user
-from chat.forms import LoginForm
+from chat.forms import LoginForm, RegisterForm
 from chat.models import User, Log
 from chat.tools import redirect_back
 from chat.plugins import db
 import uuid
 #系统授权
 bp_auth = Blueprint('auth', __name__)
+@bp_auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        code = form.code.data
+        email = form.email.data
+        password = form.password.data
+        user = User(
+            id=uuid.uuid4().hex,
+            code=code.lower(),
+            name=code.lower(),
+            email=email.lower()
+        )
+        user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+        flash('注册成功!')
+    return render_template('auth/register.html', form=form)
 @bp_auth.route('/login', methods = ['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
